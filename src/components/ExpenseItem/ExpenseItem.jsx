eb5ac1589f615fc7f63d6acbf04076e358be974d
cristhian <crishuamni023@gmail.com>
2025-07-11 14:37:02 -0500

    implementacion de motion con videCoding

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,16 +1,21 @@
-
 import './ExpenseItem.css';
-const ExpenseItem = ({ description, amount, date, onDelete ,id}) => {
-    return (
-    <li className="expense-item">
-  <span>
-    {description} - ${amount} en {date}
-  </span>
-  <button className="delete-btn" onClick={() => onDelete(id)}>❌</button>
-</li>
+import { motion } from "framer-motion";
 
+const ExpenseItem = ({ description, amount, date, onDelete, id }) => {
+    return (
+        <motion.li
+            className="expense-item"
+            initial={{ opacity: 0, scale: 0.8, x: 50 }}
+            animate={{ opacity: 1, scale: 1, x: 0 }}
+            exit={{ opacity: 0, scale: 0.8, x: 50 }}
+            transition={{ duration: 0.25 }}
+        >
+            <span>
+                {description} - ${amount} en {date}
+            </span>
+            <button className="delete-btn" onClick={() => onDelete(id)}>❌</button>
+        </motion.li>
     );
-    
 };
 
 export default ExpenseItem;
--- src/components/ExpenseList/ExpenseList.jsx
+++ src/components/ExpenseList/ExpenseList.jsx
@@ -1,27 +1,29 @@
 import { ExpenseItem } from "../index.jsx";
 import './ExpenseList.css';
+import { AnimatePresence } from "framer-motion";
+
 const ExpenseList = ({ expenses, onDelete }) => {
     return (
-    
-<ul className="chill2-list">
-  <div className="expenses-header">
-    <h2 className="expenses-title">Jul</h2>
-    <h3 className="expenses-title">2025</h3>
-    <p className="expenses-date">Julio 2025</p>
-    <p className="expenses-subtitle">Minimalism style</p>
-  </div>
-  
-  {expenses.map((expense) => (
-    <ExpenseItem
-      key={expense.id}
-      id={expense.id}
-      description={expense.description}
-      amount={expense.amount}
-      date={expense.date}
-      onDelete={onDelete}
-    />
-  ))}
-</ul>
+        <ul className="chill2-list">
+            <div className="expenses-header">
+                <h2 className="expenses-title">Jul</h2>
+                <h3 className="expenses-title">2025</h3>
+                <p className="expenses-date">Julio 2025</p>
+                <p className="expenses-subtitle">Minimalism style</p>
+            </div>
+            <AnimatePresence>
+                {expenses.map((expense) => (
+                    <ExpenseItem
+                        key={expense.id}
+                        id={expense.id}
+                        description={expense.description}
+                        amount={expense.amount}
+                        date={expense.date}
+                        onDelete={onDelete}
+                    />
+                ))}
+            </AnimatePresence>
+        </ul>
     );
 };
 
